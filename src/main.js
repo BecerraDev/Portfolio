a9b018719e07769862dc87370359a61adc1d4b07
Byron Becerra <byron.becerra.aravena@gmail.com>
2024-10-09 20:19:22 -0300

    Delete src directory

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import router from './router';
-import './styles.css'; // Si styles.css está en src
-
-createApp(App)
-  .use(router)
-  .mount('#app');
